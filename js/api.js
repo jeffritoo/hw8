4b05d34ade979344a58629c341762907d989e52a
jeffritoo <jeffryvdavid@gmail.com>
2021-04-03 14:29:45 -0700

    2:29 PM

---
+++ js/api.js
@@ -0,0 +1,31 @@
+// Fetch data from the API
+fetch("https://thejsway-server.herokuapp.com/api/articles")
+  .then(response => response.json()) // Translate JSON into JavaScript
+
+  .then(articles => {
+
+    articles.forEach(article => {
+
+      // Create title element
+      const titleElement = document.createElement("h3");
+
+      titleElement.textContent = article.title;
+      // Create content element
+      const contentElement = document.createElement("p");
+
+      contentElement.textContent = article.content;
+
+      // Add title and content to the page
+      const articlesElement = document.getElementById("articles");
+
+      articlesElement.appendChild(titleElement);
+
+      articlesElement.appendChild(contentElement);
+
+    });
+  })
+
+  .catch(err => {
+      
+    console.error(err.message);
+  });
